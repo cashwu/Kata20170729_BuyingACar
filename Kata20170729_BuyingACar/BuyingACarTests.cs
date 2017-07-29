fee5df8ec42ae3e43d3be2accf29c5bd6fe0b86b
cash <cash@cashwu.com>
2017-07-29 21:51:14 +0800

    old car 12000, new car 8000, save 1000, loss 1.5

---
+++ Kata20170729_BuyingACar/BuyingACarTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata20170729_BuyingACar
+{
+    [TestClass]
+    public class BuyingACarTests
+    {
+        [TestMethod]
+        public void oldcar_12000_newcar_8000_save_1000_loss_1point5()
+        {
+            var buyCar = new BuyCar();
+            var actual = buyCar.nbMonths(12000, 8000, 1000, 1.5);
+            AssertBuyingCarLeftMoney(new[] {0, 4000}, actual);
+        }
+
+        private static void AssertBuyingCarLeftMoney(int[] expected, int[] actual)
+        {
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+
+    public class BuyCar
+    {
+        public int[] nbMonths(int startPriceOld, int startPriceNew, int savingperMonth, double percentLossByMonth)
+        {
+            var leftMoney = 0;
+            if (startPriceOld > startPriceNew)
+            {
+                leftMoney = startPriceOld - startPriceNew;
+            }
+
+            return new[] { 0, leftMoney };
+        }
+    }
+}
