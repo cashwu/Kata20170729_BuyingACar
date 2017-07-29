5d72811ca5af81e740bf30e6087dd0b43bde1a50
cash <cash@cashwu.com>
2017-07-29 22:05:39 +0800

    old car 7000, newcar 8000, save 1000, loss 1.5

--- Kata20170729_BuyingACar/BuyingACarTests.cs
+++ Kata20170729_BuyingACar/BuyingACarTests.cs
@@ -15,6 +15,14 @@ namespace Kata20170729_BuyingACar
             AssertBuyingCarLeftMoney(new[] {0, 4000}, actual);
         }
 
+        [TestMethod]
+        public void oldcar_7000_newcar_8000_save_1000_loss_1point5()
+        {
+            var buyCar = new BuyCar();
+            var actual = buyCar.nbMonths(7000, 8000, 1000, 1.5);
+            AssertBuyingCarLeftMoney(new []{ 1, 15 }, actual);
+        }
+
         private static void AssertBuyingCarLeftMoney(int[] expected, int[] actual)
         {
             CollectionAssert.AreEqual(expected, actual);
@@ -26,12 +34,23 @@ namespace Kata20170729_BuyingACar
         public int[] nbMonths(int startPriceOld, int startPriceNew, int savingperMonth, double percentLossByMonth)
         {
             var leftMoney = 0;
+            var leftMonth = 0;
+
             if (startPriceOld > startPriceNew)
             {
                 leftMoney = startPriceOld - startPriceNew;
+                return new[] { leftMonth, leftMoney };
             }
 
-            return new[] { 0, leftMoney };
+            startPriceOld = (int)(startPriceOld * (100 - 1.5) / 100);
+            startPriceNew = (int) (startPriceNew * (100 - 1.5) / 100);
+            if (startPriceOld + savingperMonth > startPriceNew)
+            {
+                leftMoney = startPriceOld + savingperMonth - startPriceNew;
+            }
+            leftMonth++;
+
+            return new[] { leftMonth, leftMoney };
         }
     }
 }
