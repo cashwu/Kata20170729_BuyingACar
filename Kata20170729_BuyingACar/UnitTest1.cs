d5582f880251f894f6b030dfb4dd96c24d44d661
cash <cash@cashwu.com>
2017-07-29 21:41:26 +0800

    Add project files.

---
+++ Kata20170729_BuyingACar/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata20170729_BuyingACar
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
